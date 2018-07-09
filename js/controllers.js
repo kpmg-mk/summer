e7536c197046d9cb29c2b7f9e9c95e75f70373b9
root <root@localhost.localdomain>
2018-07-09 16:02:23 +0300

    commit

--- js/controllers.js
+++ js/controllers.js
@@ -12,7 +12,22 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+    $scope.$on('$ionicView.loaded', function(event) {
+        cameraButton= document.getElementById("home-button1");
+        fileInput = document.getElementById('file-input');
 
+        fileInput.addEventListener('change', function () {
+        let photo = fileInput.files[0];
+
+        let formData = new FormData();
+        formData.append("photo", photo);        
+        // formData.append("user", JSON.stringify(user));   // you can add also some json data to formData like e.g. user = {name:'john', age:34}
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", 'http://25bdd36a.ngrok.io/upload/');
+        xhr.send(formData);
+    });
+    });
 
 }])
    
