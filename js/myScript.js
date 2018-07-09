b3ab3cf2337438b8172b3c5a38227b65240b9ce7
root <root@localhost.localdomain>
2018-07-09 12:38:57 +0300

    commit

--- js/myScript.js
+++ js/myScript.js
@@ -3,6 +3,18 @@ const fileInput = document.getElementById('file-input');
 
 
 fileInput.addEventListener('change', function () {
+    let photo = fileInput.files[0];
+
+    let formData = new FormData();
+    formData.append("photo", photo);        
+    // formData.append("user", JSON.stringify(user));   // you can add also some json data to formData like e.g. user = {name:'john', age:34}
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://c863c70b.ngrok.io/upload/');
+    xhr.send(formData);
+
+
+
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
