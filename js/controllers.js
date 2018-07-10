ff2dfa7520b601ae205acd333e55515d42594365
root <root@localhost.localdomain>
2018-07-10 15:31:21 +0300

    commit

--- js/controllers.js
+++ js/controllers.js
@@ -19,13 +19,14 @@ function ($scope, $stateParams) {
         cameraButton= document.getElementById("home-button1");
         fileInput = document.getElementById('file-input');
         getBase64= " ";
-        var reader = new FileReader();
+        
         
         fileInput.addEventListener('change', function () {
                 getBase64= function (file) {
+                    var reader = new FileReader();
                     reader.readAsDataURL(file);
                     reader.onload = function () {
-                    console.log(reader.result);
+                    //console.log(reader.result);
                     //console.log(reader);
                         return reader;
                     };
@@ -37,8 +38,28 @@ function ($scope, $stateParams) {
             alert("SENDING");
             let photo = fileInput.files[0];
 
-            b64String= getBase64(photo);
+            //b64String= getBase64(photo);
+
+            var reader = new FileReader();
+            reader.readAsDataURL(photo);
+            reader.onload = function () {
+                //console.log(reader.result);
+                //console.log(reader);
+
+                let formData = new FormData();
+                formData.append("bString", reader.result);        
+                // formData.append("user", JSON.stringify(user));   // you can add also some json data to formData like e.g. user = {name:'john', age:34}
+
+                let xhr = new XMLHttpRequest();
+                pathURL= 'https://cf7856b0.ngrok.io/upload/';
+                xhr.open("POST", pathURL);
+                xhr.send(JSON.stringify(reader.result));
+            };
+            reader.onerror = function (error) {
+                console.log('Error: ', error);
+            };
             
+            /*
             let formData = new FormData();
             formData.append("b64String", b64String);        
             // formData.append("user", JSON.stringify(user));   // you can add also some json data to formData like e.g. user = {name:'john', age:34}
@@ -47,6 +68,7 @@ function ($scope, $stateParams) {
             pathURL= 'https://cf7856b0.ngrok.io/upload/';
             xhr.open("POST", pathURL);
             xhr.send(JSON.stringify(b64String.result));
+            */
         });
     });
 
