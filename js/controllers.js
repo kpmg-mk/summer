688f81d0c31cca19fb40e65a819fda8d8a020433
root <root@localhost.localdomain>
2018-07-10 11:49:46 +0300

    commit

--- js/controllers.js
+++ js/controllers.js
@@ -13,21 +13,35 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
     $scope.$on('$ionicView.loaded', function(event) {
+        function getBase64(file) {
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function () {
+              console.log(reader.result);
+            };
+            reader.onerror = function (error) {
+              console.log('Error: ', error);
+            };
+         }
+
+
         cameraButton= document.getElementById("home-button1");
         fileInput = document.getElementById('file-input');
-        /*
+        
         fileInput.addEventListener('change', function () {
             alert("SENDING");
-        let photo = fileInput.files[0];
+            let photo = fileInput.files[0];
 
-        let formData = new FormData();
-        formData.append("photo", photo);        
-        // formData.append("user", JSON.stringify(user));   // you can add also some json data to formData like e.g. user = {name:'john', age:34}
+            getBase64(file);
+            /*
+            let formData = new FormData();
+            formData.append("photo", photo);        
+            // formData.append("user", JSON.stringify(user));   // you can add also some json data to formData like e.g. user = {name:'john', age:34}
 
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", 'https://cf7856b0.ngrok.io/upload/');
-        xhr.send(formData);
-    });*/
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", 'https://cf7856b0.ngrok.io/upload/');
+            xhr.send(formData);*/
+        });
     });
 
 }])
